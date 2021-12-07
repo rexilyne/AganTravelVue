d74129d6d145df77a5fd204b6bb1d6c11873a110
ASUS <tfadriangawa@gmail.com>
2021-12-07 13:24:54 +0700

    Routing

---
+++ src/components/Layout/NavbarLayout.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <v-toolbar app fixed height="90" class="blue darken-4">
+            <v-row>
+                <v-col md="3" class="text-h4 font-weight-bold yellow--text">
+                    <div role="button" @click="home">
+                        AganTravel.com
+                    </div>
+                </v-col>
+                <v-col md="1">
+                    <div role="button" class="text-h6 mt-1 white--text" @click="bus">
+                        Bus
+                    </div>
+                </v-col >
+                <v-col md="1">
+                    <div role="button" class="text-h6 mt-1 white--text" @click="kereta">
+                        Kereta
+                    </div>
+                </v-col>
+                <v-col md="1">
+                    <div role="button" class="text-h6 mt-1 white--text" @click="pesawat">
+                        Pesawat
+                    </div>
+                </v-col>
+                <v-col md="6 text-right">
+                    <v-btn class="mt-1 mr-2" outlined color="white" @click="register">Register</v-btn>
+                    <v-btn class="mt-1" outlined color="white" @click="login">Login</v-btn>
+                </v-col>
+            </v-row>
+            <!-- <v-app-bar-title>TokoLaku.id</v-app-bar-title> -->
+            <!-- <v-spacer> -->
+        </v-toolbar>
+        <div class="pa-5">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        methods: {
+            home() {
+                this.$router.push({
+                    name: 'Home',
+                });
+            },
+
+            bus() {
+                this.$router.push({
+                    name: 'List Bus',
+                });
+            },
+
+            kereta() {
+                this.$router.push({
+                    name: 'List Kereta',
+                });
+            },
+
+            pesawat() {
+                this.$router.push({
+                    name: 'List Pesawat',
+                });
+            },
+
+            register() {
+                this.$router.push({
+                    name: 'Register',
+                });
+            },
+
+            login() {
+                this.$router.push({
+                    name: 'Login',
+                });
+            }
+        }
+    }
+</script>
--- src/router.js
+++ src/router.js
@@ -11,12 +11,127 @@ const router = new VueRouter({
     mode: "history",
     routes: [
         {
-            //path
-            //name
-            //component
-            //children
+            path: '/home',
+            name: 'Home',
+            meta: { title: 'Home' },
+            component: importComponent('Home'),
+        },
+
+        {
+            path: '/authentication',
+            // redirect: '/authentication/login',
+            component: importComponent('Layout/NavbarLayout'),
+            children: [
+                // Login
+                {
+                    path: '/authentication/login',
+                    name: 'Login',
+                    meta: { title: 'Login' },
+                    component: importComponent('Authentication/Login')
+                },
+
+                // Register
+                {
+                    path: '/authentication/register',
+                    name: 'Register',
+                    meta: { title: 'Register' },
+                    component: importComponent('Authentication/Register')
+                }
+            ]
+        },
+
+        {
+            path: '/user',
+            component: importComponent('Layout/DashboardLayout'),
+            children: [
+                // Profile
+                {
+                    path: '/user/profile',
+                    name: 'Profile',
+                    meta: { title: 'Profile' },
+                    component: importComponent('User/Profile')
+                },
+
+                // Order Bus
+                {
+                    path: '/user/orderbus',
+                    name: 'Order Bus',
+                    meta: { title: 'Order Bus' },
+                    component: importComponent('User/OrderBus')
+                },
+
+                // Order Kereta
+                {
+                    path: '/user/orderkereta',
+                    name: 'Order Kereta',
+                    meta: { title: 'Order Kereta' },
+                    component: importComponent('User/OrderKereta')
+                },
+
+                // Order Pesawat
+                {
+                    path: '/user/orderpesawat',
+                    name: 'Order Pesawat',
+                    meta: { title: 'Order Pesawat' },
+                    component: importComponent('User/OrderPesawat')
+                }
+            ]
+        },
+
+        {
+            path: '/bus',
+            component: importComponent('Layout/NavbarLayout'),
+            children: [
+                // List Bus
+                {
+                    path: '/bus/listbus',
+                    name: 'List Bus',
+                    meta: { title: 'List Bus' },
+                    component: importComponent('Bus/ListBus')
+                }
+            ]
+        },
+
+        {
+            path: '/kereta',
+            component: importComponent('Layout/NavbarLayout'),
+            children: [
+                // List Bus
+                {
+                    path: '/kereta/listkereta',
+                    name: 'List Kereta',
+                    meta: { title: 'List Kereta' },
+                    component: importComponent('Kereta/ListKereta')
+                }
+            ]
+        },
+
+        {
+            path: '/pesawat',
+            component: importComponent('Layout/NavbarLayout'),
+            children: [
+                // List Bus
+                {
+                    path: '/pesawat/listpesawat',
+                    name: 'List Pesawat',
+                    meta: { title: 'List Pesawat' },
+                    component: importComponent('Pesawat/ListPesawat')
+                }
+            ]
+        },
+
+ 
+        {
+            path: '*',
+            redirect: '/home'
         }
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title;
+    next();
+});
+
 
 export default router;
