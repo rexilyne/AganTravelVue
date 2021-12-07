6be2531a95bdd308db9ce194ffa67b548fb32c2f
ASUS <tfadriangawa@gmail.com>
2021-12-07 09:02:41 +0700

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+function importComponent(path) {
+    return () => import(`./components/${path}.vue`);
+}
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            //path
+            //name
+            //component
+            //children
+        }
+    ]
+});
+
+export default router;
